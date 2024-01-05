ac6e0f9d9c141a9d00f63359aff9f207e6eddab2
papastepan <papastepano@gmail.com>
2024-01-05 20:25:21 +0100

    spell cheker and string helper module

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,29 @@
-# Write your solution here
+# write your solution here
+import difflib 
+user_input = input("Write text: ")
+suggestions = {}
+
+with open("wordlist.txt") as wordlist:
+    word_list = []
+    for word in wordlist:
+        word = word.replace("\n", "")
+        word_list.append(word.lower())
+
+misspelled_words = []
+input_array = user_input.split(" ")
+
+for i in input_array:
+    if i.lower() not in word_list:
+        misspelled_words.append(i)
+
+for res in input_array:
+    if res not in misspelled_words:
+        print(f"{res} ", end="")
+    else:
+        print(f"*{res}* ", end="")
+        suggestions[res] = difflib.get_close_matches(res, word_list)
+
+print("\nsuggestions:")
+for key, value in suggestions.items():
+    print(f"\n{key}: ", end="")
+    print(", ".join(value))
--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,38 @@
 # Write your solution here
+def change_case(orig_string: str):
+    new_string = ''
+    for char in orig_string:
+        if char.islower():
+            new_string += char.upper()
+        else:
+            new_string += char.lower()
+    return new_string
+
+def split_in_half(orig_string: str):
+    original_length = len(orig_string)
+    half_length = original_length // 2
+    first_half = orig_string[:half_length]
+    second_half = orig_string[half_length:]
+    return first_half, second_half
+    pass
+
+def remove_special_characters(orig_string: str):
+    special_chars = "!§¤@#$%^&*()[]{};:,./<>?\|`~-=_+"
+    for char in special_chars:
+        orig_string = orig_string.replace(char, "")
+    return orig_string
+
+if __name__ == "__main__":
+    import string_helper
+
+    my_string = "Well hello there!"
+
+    print(string_helper.change_case(my_string))
+
+    p1, p2 = string_helper.split_in_half(my_string)
+
+    print(p1)
+    print(p2)
+
+    m2 = string_helper.remove_special_characters("This is a test, lets see how it goes!!!11!")
+    print(m2)
