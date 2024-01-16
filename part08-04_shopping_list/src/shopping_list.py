63cb82bed2f17131ac00e5ec37ea36dbc0871fce
papastepan <papastepano@gmail.com>
2024-01-16 14:51:12 +0100

    advanced classes and objects

--- part08-04_shopping_list/src/shopping_list.py
+++ part08-04_shopping_list/src/shopping_list.py
@@ -19,3 +19,16 @@ class ShoppingList:
 # -------------------------
 # Write your solution here:
 # -------------------------
+def total_units(my_list: ShoppingList):
+    total = 0
+    for item in my_list.products:
+        total += item[1]
+    return total
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("pineapple", 1)
+
+    print(total_units(my_list))
