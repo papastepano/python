5ea409acb7221f90cc5156b69f9bb95e6e0fec47
papastepan <papastepano@gmail.com>
2024-01-17 16:38:25 +0100

    Defining methods

--- part08-10_decreasing_counter/src/decreasing_counter.py
+++ part08-10_decreasing_counter/src/decreasing_counter.py
@@ -2,11 +2,28 @@
 class DecreasingCounter:
     def __init__(self, initial_value: int):
         self.value = initial_value
+        self.init = initial_value
 
     def print_value(self):
         print("value:", self.value)
 
     def decrease(self):
-        pass
+        if self.value > 0:
+            self.value -= 1
 
     # Write the rest of the methods here!
+    def set_to_zero(self):
+        self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.init
+
+if __name__ == "__main__":
+    counter = DecreasingCounter(55)
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.print_value()
+    counter.reset_original_value()
+    counter.print_value()
--- part08-12_number_stats/src/number_stats.py
+++ part08-12_number_stats/src/number_stats.py
@@ -2,9 +2,42 @@
 class  NumberStats:
     def __init__(self):
         self.numbers = 0
+        self.sum = 0
 
     def add_number(self, number:int):
-        pass
+        self.numbers += 1
+        self.sum += number
 
     def count_numbers(self):
-        pass
+        return self.numbers
+
+    def get_sum(self):
+        if self.numbers == 0:
+            return 0
+        else:
+            return self.sum
+
+    def average(self):
+        if self.numbers == 0:
+            return 0
+        else:
+            return self.sum/self.numbers
+        
+stats = NumberStats()
+even = NumberStats()
+odd = NumberStats()
+while True:
+    a = int(input("Please type in integer numbers:"))
+    if a == -1:
+        break
+    else:
+        stats.add_number(a)
+        if a%2==0:
+            even.add_number(a)
+        else:
+            odd.add_number(a)
+
+print("Sum of numbers:", stats.get_sum())
+print("Mean of numbers:", stats.average())
+print("Sum of even numbers:", even.get_sum())
+print("Sum of odd numbers:", odd.get_sum())
