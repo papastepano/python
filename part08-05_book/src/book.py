680160c0c54926d973a13259cecf8b533d0562c2
papastepan <papastepano@gmail.com>
2024-01-17 13:20:51 +0100

    defining classes

--- part08-05_book/src/book.py
+++ part08-05_book/src/book.py
@@ -1,1 +1,11 @@
 # Write your solution here:
+class Book:
+    def __init__(self, name: str, author: str, genre: str, year: int):
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.year = year
+
+    # def __str__(self):
+    #     return f"{self.name} ({self.year}) - {self.author}, {self.genre}"
+        
