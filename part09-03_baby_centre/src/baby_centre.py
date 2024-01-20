9746d748d5c50e96312605d6d7bba1376b17a0da
papastepan <papastepano@gmail.com>
2024-01-20 10:20:40 +0100

    Objects and references part I

--- part09-03_baby_centre/src/baby_centre.py
+++ part09-03_baby_centre/src/baby_centre.py
@@ -15,4 +15,32 @@ class BabyCentre:
 
     def weigh(self, person: Person):
         # return the weight of the person passed as an argument
-        return -1
+        self.number_of_weigh_ins += 1
+        return person.weight
+    
+    def feed(self, person: Person):
+        # increase the weight of the person passed as an argument
+        person.weight += 1
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
+
+if __name__ == "__main__":
+    baby_centre = BabyCentre()
+
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
