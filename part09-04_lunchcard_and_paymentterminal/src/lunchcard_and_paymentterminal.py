3e6276423ac900e4963af479edeb5602eaebbc05
papastepan <papastepano@gmail.com>
2024-01-21 12:00:29 +0100

    Objects and references part 09

--- part09-04_lunchcard_and_paymentterminal/src/lunchcard_and_paymentterminal.py
+++ part09-04_lunchcard_and_paymentterminal/src/lunchcard_and_paymentterminal.py
@@ -8,7 +8,12 @@ class LunchCard:
         self.balance += amount
 
     def subtract_from_balance(self, amount: float):
-        pass
+        self.balance -= amount
+        if self.balance >= 0:
+            return True
+        else:
+            self.balance += amount
+            return False
         # The amount should be subtracted from the balance only if there is enough money on the card
         # If the payment is successful, the method returns True, and otherwise it returns False
 
@@ -25,27 +30,70 @@ class PaymentTerminal:
         # increase the number of lunches sold, and return the appropriate change.
         # If the payment passed as an argument is not large enough to cover the price,
         # the lunch is not sold, and the entire sum is returned.
-        pass
+        if payment >= 2.50:
+            self.lunches += 1
+            self.funds += 2.50
+            return payment - 2.50
+        else:
+            return payment
 
-   def eat_special(self, payment: float):
+
+    def eat_special(self, payment: float):
         # A special lunch costs 4.30 euros.
         # Increase the value of the funds at the terminal by the price of the lunch,
         # increase the number of specials sold, and return the appropriate change.
         # If the payment passed as an argument is not large enough to cover the price,
         # the lunch is not sold, and the entire sum is returned.
-        pass
+        if payment >= 4.30:
+            self.specials += 1
+            self.funds += 4.30
+            return payment - 4.30
+        else:
+            return payment
+
 
     def eat_lunch_lunchcard(self, card: LunchCard):
         # A regular lunch costs 2.50 euros.
         # If there is enough money on the card, subtract the price of the lunch from the balance
         # and return True. If not, return False.
-        pass
+        card.balance -= 2.50
+        if card.balance >= 0:
+            self.lunches += 1
+            return True
+        else:
+            card.balance += 2.50
+            return False
+
 
     def eat_special_lunchcard(self, card: LunchCard):
         # A special lunch costs 4.30 euros.
         # If there is enough money on the card, subtract the price of the lunch from the balance
         # and return True. If not, return False.
-        pass
-
+        card.balance -= 4.30
+        if card.balance >= 0:
+            self.specials += 1
+            return True
+        else:
+            card.balance += 4.30
+            return False
+        
     def deposit_money_on_card(self, card: LunchCard, amount: float):
-        pass
+        card.balance += amount
+        self.funds -= amount
+
+
+if __name__ == "__main__":
+    exactum = PaymentTerminal()
+
+    change = exactum.eat_lunch(10)
+    print("The change returned was", change)
+
+    change = exactum.eat_lunch(5)
+    print("The change returned was", change)
+
+    change = exactum.eat_special(4.3)
+    print("The change returned was", change)
+
+    print("Funds available at the terminal:", exactum.funds)
+    print("Regular lunches sold:", exactum.lunches)
+    print("Special lunches sold:", exactum.specials)
--- part09-05_comparing_properties/src/comparing_properties.py
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -5,3 +5,19 @@ class RealProperty:
         self.rooms = rooms
         self.square_metres = square_metres
         self.price_per_sqm = price_per_sqm
+
+    def bigger(self, compared_to):
+        return self.square_metres > compared_to.square_metres
+
+    def price_difference(self, compared_to):
+        total_price_self = self.square_metres * self.price_per_sqm
+        total_price_compared_to = compared_to.square_metres * compared_to.price_per_sqm
+        return abs(total_price_self - total_price_compared_to)
+
+    def more_expensive(self, compared_to):
+        total_price_self = self.price_per_sqm * self.square_metres
+        total_price_compared_to = compared_to.price_per_sqm * compared_to.square_metres
+        if total_price_self > total_price_compared_to:
+            return True
+        else:
+            return False
