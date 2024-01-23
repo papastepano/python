a3cb6a8fb360ff5034fd80d4102502b230f2b9b0
papastepan <papastepano@gmail.com>
2024-01-23 18:37:26 +0100

    Encapsulation

--- part09-09_car/src/car.py
+++ part09-09_car/src/car.py
@@ -1,1 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__petrol = 60
+        self.__odometer = 0
+
+    def fill_up(self):
+        self.__petrol = 60
+    
+    def drive(self, km: int):
+        if km > self.__petrol:
+            self.__odometer += self.__petrol
+            self.__petrol = 0
+        elif km < 0:
+            return
+        else:
+            self.__odometer += km
+            self.__petrol -= km
+
+    def __str__(self):
+        return f"Car: odometer reading {self.__odometer} km, petrol remaining {self.__petrol} litres"
