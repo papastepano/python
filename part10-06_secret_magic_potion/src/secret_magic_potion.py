c4230b76d5826c8a70631f280cd3ea40b63032da
papastepan <papastepano@gmail.com>
2024-01-25 14:04:09 +0100

    access modifiers

--- part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -12,4 +12,30 @@ class MagicPotion:
         for ingredient in self._ingredients:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name: str, password: str):
+        super().__init__(name)
+        self._password = password
 
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if password == self._password:
+            super().add_ingredient(ingredient, amount)
+        else:
+            raise ValueError("Wrong password!")
+
+    def print_recipe(self, password: str):
+        if password == self._password:
+            super().print_recipe()
+        else:
+            raise ValueError("Wrong password!")
+
+
+
+if __name__ == "__main__":
+    diminuendo = SecretMagicPotion("Diminuendo maximus", "hocuspocus")
+    diminuendo.add_ingredient("Toadstool", 1.5, "hocuspocus")
+    diminuendo.add_ingredient("Magic sand", 3.0, "hocuspocus")
+    diminuendo.add_ingredient("Frogspawn", 4.0, "hocuspocus")
+    diminuendo.print_recipe("hocuspocus")
+
+    diminuendo.print_recipe("pocushocus") # WRONG password!
