6bf904bdb45e8eda812e431d931069b6d6966edc
papastepan <papastepano@gmail.com>
2024-01-28 11:27:46 +0100

    Object oriented programming techniques I

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,51 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
-
+        self.__euros = euros
+        self.__cents = cents
+    
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        if self.__cents < 10:
+            return f"{self.__euros}.0{self.__cents} eur"
+        else:
+            return f"{self.__euros}.{self.__cents} eur"
+
+    def __repr__(self) -> str:
+        pass
+
+    def __eq__(self, another):
+        return self.__euros == another.__euros and self.__cents == another.__cents
+    
+    def __gt__(self, another):
+        return self.__euros > another.__euros or (self.__euros == another.__euros and self.__cents > another.__cents)
+    
+    def __lt__(self, another):
+        return self.__euros < another.__euros or (self.__euros == another.__euros and self.__cents < another.__cents)
+    
+    def __ne__(self, another):
+        return not self.__eq__(another)
+    
+    def __add__(self, another):
+        euros = self.__euros + another.__euros
+        cents = self.__cents + another.__cents
+        if cents >= 100:
+            euros += 1
+            cents -= 100
+        return Money(euros, cents)
+
+
+    def __sub__(self, another):
+        if self.__euros < another.__euros or (self.__euros == another.__euros and self.__cents < another.__cents):
+            raise ValueError("a negative result is not allowed")
+        euros = self.__euros - another.__euros
+        cents = self.__cents - another.__cents
+        if cents < 0:
+            euros -= 1
+            cents += 100
+        return Money(euros, cents)
+
+if __name__ == "__main__":
+    e1 = Money(4, 5)
+    print(e1)
+    e1.euros = 1000
+    print(e1)
--- part10-08_simple_date/src/simple_date.py
+++ part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,74 @@
 # WRITE YOUR SOLUTION HERE:
+class SimpleDate:
+    def __init__(self, day: int, month: int, year: int):
+        self.day = day
+        self.month = month
+        self.year = year
+        self.combined = int(f"{year}{month:02d}{day:02d}")
 
+
+    def __str__(self):
+        return f"{self.day}.{self.month}.{self.year}"
+
+    def __gt__(self, other: "SimpleDate") -> bool:
+        if self.combined > other.combined:
+            return True
+        else:
+            return False
+
+    def __le__(self, other: "SimpleDate") -> bool:
+        if self.combined <= other.combined:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other: "SimpleDate") -> bool:
+        if self.combined < other.combined:
+            return True
+        else:
+            return False
+        
+
+    def __ne__(self, other: "SimpleDate") -> bool:
+        if self.combined != other.combined:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other: "SimpleDate") -> bool:
+        if self.combined == other.combined:
+            return True
+        else:
+            return False
+        
+    def __add__(self, days: int) -> "SimpleDate":
+        total_days = self.day + days
+
+        new_day = total_days % 30
+        new_month = self.month + (total_days // 30)
+
+        if new_day == 0:
+            new_day = 30
+            new_month -= 1
+
+        new_year = self.year + (new_month - 1) // 12
+        new_month = new_month % 12 if new_month % 12 != 0 else 12
+
+        return SimpleDate(new_day, new_month, new_year)
+
+    def __sub__(self, other: "SimpleDate") -> int:
+        total_days = self.year*360 + self.month*30 + self.day
+        other_days = other.year*360 + other.month*30 + other.day
+
+        return abs(total_days - other_days)
+
+
+if __name__ == "__main__":
+    
+    d1 = SimpleDate(4, 10, 2020)
+    d2 = SimpleDate(2, 11, 2020)
+    d3 = SimpleDate(28, 12, 1985)
+
+    print(d2-d1)
+    print(d1-d2)
+    print(d1-d3)
