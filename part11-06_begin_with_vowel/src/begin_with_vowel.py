6c0f9f4400b640d73fce3b547610a3b28db04697
Stepan Papazyan <papastepano@gmail.com>
2024-02-05 11:21:33 +0200

    refactor

--- part11-06_begin_with_vowel/src/begin_with_vowel.py
+++ part11-06_begin_with_vowel/src/begin_with_vowel.py
@@ -1,6 +1,6 @@
 # WRITE YOUR SOLUTION HERE:
 def begin_with_vowel(words: list):
-    return [word for word in words if word[0] in "aeiouAEIOU"]
+    return [word for word in words if word[0].lower() in "aeiou"]
 
 
 if __name__ == "__main__":
