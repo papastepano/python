79d44fd0964e1a2af66ef4840c28ce38f9f308d9
Stepan Papazyan <papastepano@gmail.com>
2024-02-15 19:37:14 +0100

    pygame animation events and more

--- part13-01_four_robots/src/main.py
+++ part13-01_four_robots/src/main.py
@@ -6,4 +6,25 @@
 # If your solution clearly does not match the exercise description, you may lose the points granted for the exercises in this part.
 
 # WRITE YOUR SOLUTION HERE:
+import pygame
 
+pygame.init()
+width_window = 640
+height_window = 480
+window = pygame.display.set_mode((width_window, height_window))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0,0,0))
+width = robot.get_width()
+height = robot.get_height()
+window.blit(robot, (0,0))
+window.blit(robot, (width_window-width,0))
+window.blit(robot, (0,height_window-height))
+window.blit(robot, (width_window-width,height_window-height))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
