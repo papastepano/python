3710f9688365ea5ed164adde208b5dfa551c1a3b
Stepan Papazyan <papastepano@gmail.com>
2024-02-18 16:27:53 +0100

    adding missing images

--- part14-01_own_game/src/main.py
+++ part14-01_own_game/src/main.py
@@ -1,1 +1,57 @@
 # Complete your game here
+import pygame
+
+class Sokoban:
+    def __init__(self):
+        pygame.init()
+        
+        self.load_images()
+        self.new_game()
+        
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.scale = self.images[0].get_width()
+
+        window_height = self.scale * self.height
+        window_width = self.scale * self.width
+        self.window = pygame.display.set_mode((window_width, window_height))
+
+        pygame.display.set_caption("Sokoban")
+
+        self.main_loop()
+
+    def load_images(self):
+        self.images = []
+        for name in ["floor", "wall", "target", "box", "robot", "done", "target_robot"]:
+            self.images.append(pygame.image.load(name + ".png"))
+
+    def new_game(self):
+        self.map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+                    [1, 2, 3, 0, 0, 0, 1, 0, 0, 1, 2, 3, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 1, 2, 3, 0, 2, 3, 0, 0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 4, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+    def main_loop(self):
+        while True:
+            self.check_events()
+            self.draw_window()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+    def draw_window(self):
+        self.window.fill((0, 0, 0))
+
+        for y in range(self.height):
+            for x in range(self.width):
+                square = self.map[y][x]
+                self.window.blit(self.images[square], (x * self.scale, y * self.scale))
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    Sokoban()
